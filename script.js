d700ad7690a8502594aaafc7fded40ec66ca54e8
Oskar <oskar.baumann@tum.de>
2020-10-30 20:15:32 +0100

    Version with restart after victory or draw

--- script.js
+++ script.js
@@ -1,14 +1,93 @@
-var gameboard = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]]
+let gameboard = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+const cellElements = document.querySelectorAll('[data-cell]')
+const htmlBoard = document.getElementById('board')
+let oTurn = true
 
+drawBoard(gameboard)
 
-
-function checkWin(){
-    for(var i = 0; i<3; i++){
-        if(gameboard[i][0] === gameboard[i][1] && gameboard[i][1] === gameboard[i][2] && gameboard[i][0] !== -1){return gameboard[i][0]}
-        if(gameboard[0][i] === gameboard[1][i] && gameboard[1][i] === gameboard[2][i] && gameboard[0][i] !== -1){return gameboard[0][i]}
+function drawBoard(board) {
+    // Draw Cells
+    for (let i = 0; i < cellElements.length; i++) {
+        let cell = cellElements[i]
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(O_CLASS)
+        if (getBoard(board, i) === 0) {
+            cell.classList.add(O_CLASS)
+            cell.removeEventListener('click', handleClick)
+        }
+        else if (getBoard(board, i) === 1) {
+            cell.classList.add(X_CLASS)
+            cell.removeEventListener('click', handleClick)
+        }
+        else {
+            cell.addEventListener('click', handleClick, { once: true })
+        }
     }
-    if(gameboard[0][0] === gameboard[1][1] && gameboard[1][1] === gameboard[2][2] && gameboard[1][1] !== -1){return gameboard[1][1]}
-    if(gameboard[2][0] === gameboard[1][1] && gameboard[1][1] === gameboard[0][2] && gameboard[1][1] !== -1){return gameboard[1][1]}
 
-    return false
+    // Set Hover State
+    if (oTurn) {
+        htmlBoard.classList.remove(X_CLASS)
+        htmlBoard.classList.add(O_CLASS)
+    }
+    else {
+        htmlBoard.classList.remove(O_CLASS)
+        htmlBoard.classList.add(X_CLASS)
+    }
+}
+
+function handleClick(e) {
+    const cell = e.target
+    const i = cell.id
+    const newClass = oTurn ? 0 : 1
+    setBoard(gameboard, i, newClass)
+    oTurn = !oTurn
+    drawBoard(gameboard)
+    if (checkWin(gameboard) >= 0) {
+        disableBoard()
+        restartGame()
+    }
+}
+
+function setBoard(board, i, val) {
+    board[Math.floor(i / 3)][i % 3] = val
+    return board
+}
+
+function getBoard(board, i) {
+    return (board[Math.floor(i / 3)][i % 3])
+}
+
+function checkWin(board) {
+    for (var i = 0; i < 3; i++) {
+        if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== -1) { return board[i][0] }
+        if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== -1) { return board[0][i] }
+    }
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] !== -1) { return board[1][1] }
+    if (board[2][0] === board[1][1] && board[1][1] === board[0][2] && board[1][1] !== -1) { return board[1][1] }
+
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (board[i][j] < 0) { return -1 }
+        }
+    }
+
+    return 2
+}
+
+function disableBoard() {
+    for (let i = 0; i < cellElements.length; i++) {
+        let cell = cellElements[i]
+        cell.removeEventListener('click', handleClick)
+    }
+}
+
+function restartGame() {
+    gameboard = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+    drawBoard(gameboard)
+}
+
+if (typeof exports !== 'undefined') {
+    module.exports = checkWin;
 }
--- script.test.js
+++ script.test.js
@@ -1,5 +1,17 @@
-const { exportAllDeclaration } = require("@babel/types")
+const checkWin = require('./script.js');
 
 test('Some Test', () => {
-    expect(1).toBe(1)
+    const gameboardBlanc = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+    const gameboardVert = [[1,-1,-1],[1,-1,-1],[1,-1,-1]];
+    const gameboardHori = [[1,1,1],[-1,-1,-1],[-1,-1,-1]];
+    const gameboardDiag = [[1,-1,-1],[-1,1,-1],[-1,-1,1]];
+    const gameboardDiag2 = [[-1,-1,0],[-1,0,-1],[0,-1,-1]];
+    const gameboardNoWin = [[1,1,0],[-1,0,1],[1,1,0]];
+
+    expect(checkWin(gameboardBlanc)).toBe(-1)
+    expect(checkWin(gameboardVert)).toBe(1)
+    expect(checkWin(gameboardHori)).toBe(1)
+    expect(checkWin(gameboardDiag)).toBe(1)
+    expect(checkWin(gameboardDiag2)).toBe(0)
+    expect(checkWin(gameboardNoWin)).toBe(-1)
 })
